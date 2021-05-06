064a93ec1b7f36fe14081ae677ba4f42f8b8e33a
rajdeep200 <69335542+rajdeep200@users.noreply.github.com>
2021-05-06 15:24:42 +0530

    finished the frontend

--- src/components/swipeButtons/swipeButtons.jsx
+++ src/components/swipeButtons/swipeButtons.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import "./swipeButtons.css"
+import React from "react";
+import ReplayIcon from "@material-ui/icons/Replay";
+import CancelRoundedIcon from "@material-ui/icons/CancelRounded";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import StarIcon from "@material-ui/icons/Star";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+import { IconButton } from "@material-ui/core";
+
+import "./swipeButtons.css";
 
 const SwipeButtons = () => {
-    return (
-        <div className="swipe__buttons">
-            Buttons
-        </div>
-    )
-}
+  return (
+    <div className="swipe__buttons">
+      <IconButton className="swipeButton__repeat">
+        <ReplayIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButton__left">
+        <CancelRoundedIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButton__star">
+        <StarIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButton__right">
+        <FavoriteIcon fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButton__lightening">
+        <FlashOnIcon fontSize="large" />
+      </IconButton>
+    </div>
+  );
+};
 
-export default SwipeButtons
+export default SwipeButtons;
